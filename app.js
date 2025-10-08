b4ca2d91b18a7316cfae2e101ee976b232a5e835
fabbernat <fabbernat@gmail.com>
2025-10-08 17:24:55 +0200

    TODO fix app.js

--- app.js
+++ app.js
@@ -8,8 +8,40 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static("public"));
 
-app.use("/cars", carsRoutes);
+app.use("/camps", carsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server runs on port ${PORT}`);
 });
+
+class mainController {
+
+  this.app = app;
+
+  app.get("/", (req, res) => {
+    res.send(200, "Üdvözlünk a nyári tábor foglaló oldalán!");
+  }
+
+  app.get("/users", (req, res) => {
+    res.send(200, "Itt az összes felhasználót megtalálod!");
+  }
+
+  app.get("/users/:id", (req, res) => {
+    const { id } = req.params;
+    res.send(200, `Itt a ${id} azonosítójú felhasználó részletes leírása!`).json();
+  }
+
+  app.get("/camps", (req, res) => {
+    res.send(200, "Itt az összes tábort megtalálod!");
+  }
+
+  app.get("/camps/:id", (req, res) => {
+    const { id } = req.params;
+    res.send(200, `Itt a ${id} azonosítójú tábor részletes leírása!`);
+  }
+
+  app.post("/camps", (req, res) => {
+    const { name, location } = req.body;
+    res.send(201, `Új tábor hozzáadva: ${name}, helyszín: ${location}`);
+  });
+}
