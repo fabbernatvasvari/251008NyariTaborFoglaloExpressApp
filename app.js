55ce458cd69458ca33779781ed003bdf8a86e4d3
fabbernat <fabbernat@gmail.com>
2025-10-08 17:28:10 +0200

    fix app.js issues, hosts

--- app.js
+++ app.js
@@ -1,47 +1,51 @@
 import express from "express";
 import cors from "cors";
-import carsRoutes from "./routes/carsRoutes.js";
+import campsRoutes from "./routes/carsRoutes.js";
 
 const PORT = 3000;
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 app.use(express.static("public"));
 
-app.use("/camps", carsRoutes);
+app.use("/camps", campsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server runs on port ${PORT}`);
 });
 
-class mainController {
-
-  this.app = app;
+class MainController {
+  constructor(app) {
+    this.app = app;
 
   app.get("/", (req, res) => {
-    res.send(200, "Üdvözlünk a nyári tábor foglaló oldalán!");
-  }
+      res.status(200).send("Üdvözlünk a nyári tábor foglaló oldalán!");
+    });
 
-  app.get("/users", (req, res) => {
-    res.send(200, "Itt az összes felhasználót megtalálod!");
-  }
+    app.get("/users", (req, res) => {
+      res.status(200).send("Itt az összes felhasználót megtalálod!");
+    });
 
-  app.get("/users/:id", (req, res) => {
-    const { id } = req.params;
-    res.send(200, `Itt a ${id} azonosítójú felhasználó részletes leírása!`).json();
-  }
+    app.get("/users/:id", (req, res) => {
+      const { id } = req.params;
+      res.status(200).send(`Itt a ${id} azonosítójú felhasználó részletes leírása!`);
+    });
 
-  app.get("/camps", (req, res) => {
-    res.send(200, "Itt az összes tábort megtalálod!");
-  }
+    app.get("/camps", (req, res) => {
+      res.status(200).send("Itt az összes tábort megtalálod!");
+    });
 
-  app.get("/camps/:id", (req, res) => {
-    const { id } = req.params;
-    res.send(200, `Itt a ${id} azonosítójú tábor részletes leírása!`);
-  }
+    app.get("/camps/:id", (req, res) => {
+      const { id } = req.params;
+      res.status(200).send(`Itt a ${id} azonosítójú tábor részletes leírása!`);
+    });
 
-  app.post("/camps", (req, res) => {
-    const { name, location } = req.body;
-    res.send(201, `Új tábor hozzáadva: ${name}, helyszín: ${location}`);
-  });
+    app.post("/camps", (req, res) => {
+      const { name, location } = req.body;
+      res.status(201).send(`Új tábor hozzáadva: ${name}, helyszín: ${location}`);
+    });
+  }
 }
+
+new MainController(app);
