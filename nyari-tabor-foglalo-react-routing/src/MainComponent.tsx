9e09fa32f88c783bffad1e189346af0fbf5c0157
fabbernat <fabbernat@gmail.com>
2025-10-09 11:53:56 +0200

    Significantly improve ui look&feel

--- nyari-tabor-foglalo-react-routing/src/MainComponent.tsx
+++ nyari-tabor-foglalo-react-routing/src/MainComponent.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { useState } from 'react';
 
 function Home() {
   return <h1>Home Page</h1>;
@@ -14,14 +15,17 @@ function Contact() {
 }
 
 export function MainComponent() {
-
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  
   return (
     <BrowserRouter>
+    <main>
       <div><Link to="/home">Nyári Tábor Foglaló</Link></div>
+
       {/* Navigation */}
       <nav>
-        <Link to="/">Home</Link> |{" "}
-        <Link to="/about">About</Link> |{" "}
+        <Link to="/">Home</Link> | {" "}
+        <Link to="/about">About</Link> | {" "}
         <Link to="/contact">Contact</Link>
       </nav>
 
@@ -40,19 +44,23 @@ export function MainComponent() {
           <li><Link to="/addcamp">Új tábor</Link></li>
           <li><Link to="/addlocation">Új táborhelyszín</Link></li>
           <li><Link to="/applications">Jelentkezéseim</Link></li>
-          @if (isLoggedIn) {
+
+          {isLoggedIn && (
             <li><Link to="/profile">Profil</Link></li>
-          }
+          )}
 
-          @if (!isLoggedIn) {
+          {!isLoggedIn && (
             <li><Link to="/login">Bejelentkezés</Link></li>
-          }
+          )}
 
-          @if (isLoggedIn) {
-            <li><Link to="#">Kijelentkezés</Link></li>
-          }
+          {isLoggedIn && (
+            <li>
+              <button onClick={() => setIsLoggedIn(false)}>Kijelentkezés</button>
+            </li>
+          )}
         </ul>
-      </nav>
+      </nav>        
+    </main>
     </BrowserRouter>
   )
 }
