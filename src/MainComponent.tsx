1cb9d47b90bd80a986793dd708ce435e0e84e0aa
fabbernatvasvari <sze4fabber@vasvari.org>
2025-10-22 11:43:24 +0200

    import 4 more pictures, it still works!

--- src/MainComponent.tsx
+++ src/MainComponent.tsx
@@ -2,6 +2,10 @@ import './MainComponent.css';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { useState } from 'react';
 import chatbotIcon from '/chatbot-icon.jpg' 
+import cserkesztabor from '/cserkesztabor.jpg'
+import drcode from '/drcode.jpg'
+import forestSummerCamp from '/forest-summer-camp.jpg'
+import childrenLyingInGrass from '/group_of_children_lying_in_the_grass_in_a_circle.jpg'
 
 function Home() {
   return <h1>Home Page</h1>;
@@ -65,6 +69,11 @@ export function MainComponent() {
               <a href="https://vite.dev" target="_blank">
                 <img src={chatbotIcon} className="logo" alt="Vite logo" />
               </a>
+              <br/>
+              <img src={cserkesztabor} className="logo" alt="Cserkésztábor" />
+              <img src={drcode} className="logo" alt="Dr. Code Tábor" />
+              <img src={forestSummerCamp} className="logo" alt="Forest Summer Camp" />
+              <img src={childrenLyingInGrass} className="logo" alt="Gyerekek a fűben" />
             </div>
           </div>
         </div>
